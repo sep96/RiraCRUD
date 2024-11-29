d548cc995636f1d0301f9b7e236f392ef65b7a68
James <sep7696@gmail.com>
2024-11-29 13:15:36 +0330

    Feat :  Add IAppDbContext

---
+++ src/RiraCRUD.Domain/Interface/IAppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RiraCRUD.Domain.Entities;
+
+namespace RiraCRUD.Domain.Interface
+{
+    public interface IAppDbContext
+    {
+        DbSet<Person> Persons { get; }
+        Task SaveChangesAsync(CancellationToken cancellation);
+    }
+}
--- src/RiraCRUD.Infrastructure/Persistence/Contexts/AppDbContext.cs
+++ src/RiraCRUD.Infrastructure/Persistence/Contexts/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using RiraCRUD.Infrastructure.Persistence.Configurations;
+﻿using RiraCRUD.Domain.Interface;
+using RiraCRUD.Infrastructure.Persistence.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,16 @@ using System.Threading.Tasks;
 
 namespace RiraCRUD.Infrastructure.Persistence.Contexts
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : DbContext , IAppDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Person> Persons { get; set; }
+
+        public async Task SaveChangesAsync(CancellationToken cancellation)
+        {
+            await base.SaveChangesAsync(cancellation);
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
